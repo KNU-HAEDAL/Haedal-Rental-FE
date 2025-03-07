af1a0a801527a6ef0f7b0034967c88da32789ce0
Daeun <dalae1009@knu.ac.kr>
2025-03-07 22:10:01 +0900

    feat(apply-form): apply-form 1, 2, 3번 컴포넌트 구현

---
+++ apps/web/src/features/apply-form/ui/ApplyStep2.tsx
@@ -0,0 +1,11 @@
+const ApplyStep2 = () => {
+    return (
+      <div>
+        <h2>2번 페이지</h2>
+        <p>2번 페이지 내용이 들어갈 자리입니다.</p>
+      </div>
+    );
+  };
+  
+  export default ApplyStep2;
+  
