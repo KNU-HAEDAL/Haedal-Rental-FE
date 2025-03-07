d05ab1c51e9d4bdc61f3b2869f9c6126d4fb747d
Dobbymin <gmin990624@gmail.com>
2025-03-07 20:39:43 +0900

    feat(main): 메인페이지 ui 구현 및 컴포넌트 분리

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
+import { MainAuthButtons, MainContainer } from '@/src/features';
+
 export default function Page() {
   return (
-    <div className='flex items-center justify-center min-h-svh'>
-      <div className='flex flex-col items-center justify-center gap-4'>
-        <h1 className='text-2xl font-bold'>메인페이지</h1>
-      </div>
+    <div className='flex flex-col items-center h-dvh text-center justify-center gap-3'>
+      <Image src='/logo/symbol-logo.png' alt='logo' width={230} height={139} />
+      <MainAuthButtons />
+      <MainContainer />
     </div>
   );
 }
