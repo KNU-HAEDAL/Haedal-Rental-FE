44f2d9c6a8d219bdd539ab55a13ff5f60187137a
Dobbymin <gmin990624@gmail.com>
2025-03-08 18:53:15 +0900

    refactor: 컴포넌트 분리 - 로그인 관련 컴포넌트를 공용 컴포넌트로 선언 - 로그인 UI 완성 시 로직 변경해야할 사항들 작성

---
+++ apps/web/shared/components/auth-buttons/AuthButtons.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Button } from '@workspace/ui/components';
+import Link from 'next/link';
+
+export const AuthButtons = () => {
+  // TODO: 로그인 UI 완성 시 해당 로직 수정
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const storedLoginState = sessionStorage.getItem('isLogin');
+    if (storedLoginState === 'true') {
+      setIsLogin(true);
+    }
+  }, []);
+
+  const onClickLogin = () => {
+    setIsLogin(true);
+    sessionStorage.setItem('isLogin', 'true');
+  };
+
+  const onClickLogout = () => {
+    setIsLogin(false);
+    sessionStorage.removeItem('isLogin');
+  };
+
+  return (
+    <div className='flex gap-2 w-[550px] justify-end'>
+      {isLogin ? (
+        <div className='flex w-full items-center justify-between'>
+          <p className='font-bold'>김해달님 반갑습니다.</p>
+          <Button variant='sun' onClick={onClickLogout}>
+            로그아웃
+          </Button>
+        </div>
+      ) : (
+        <>
+          <Link href='/signup'>
+            <Button variant='ghost'>회원가입</Button>
+          </Link>
+          <Link href='/login'>
+            <Button variant='moon' onClick={onClickLogin}>
+              로그인
+            </Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
